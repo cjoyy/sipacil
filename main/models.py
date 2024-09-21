bfc2c868e6839eb9a97a2ab2473771291fce1f39
cjoyy <joycalvin1604@gmail.com>
2024-09-21 18:45:53 +0700

    update tugas 4

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 import uuid
+from django.contrib.auth.models import User
 
 class ProductEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main/views.py
+++ main/views.py
@@ -3,14 +3,58 @@ from main.forms import ProductEntryForm
 from main.models import ProductEntry
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+@login_required(login_url='/login')
 def show_main(request):
-    product_entries = ProductEntry.objects.all()
+    product_entries = ProductEntry.objects.filter(user=request.user)
     context = {
         'npm' : '2306244974',
-        'name': 'Calvin Joy Tarigan',
+        'name': request.user.username,
         'class': 'PBP C',
-        'product_entries' : product_entries
+        'product_entries' : product_entries,
+        'last_login': request.COOKIES['last_login'],
+        
     }
 
     return render(request, "main.html", context)
@@ -19,7 +63,9 @@ def create_product_entry(request):
     form = ProductEntryForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product_entry = form.save(commit=False)
+        product_entry.user = request.user
+        product_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
