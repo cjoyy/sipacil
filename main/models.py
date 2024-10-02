dfc32fc0d6759535c0c95cc54d4f5cb3b5fdbe77
cjoyy <joycalvin1604@gmail.com>
2024-10-02 11:20:48 +0700

    update tugas 5

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class ProductEntry(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -8,3 +9,9 @@ class ProductEntry(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)  # Rating dari 0.00 sampai 9.99
+    date = models.DateTimeField(default=timezone.now)  # Tanggal produk ditambahkan
+    is_avail = models.BooleanField(default=True)  # Status ketersediaan produk
+
+    def __str__(self):
+        return self.name
